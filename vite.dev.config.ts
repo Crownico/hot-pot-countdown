e2053aeef0ed4bbf4316e1a7863817ac1b24ee41
Crownico <1396612862@qq.com>
2023-11-27 00:59:05 +0800

    fix: 🧩 修复按需导入下ElMessage等API的类型检查问题

--- vite.dev.config.ts
+++ vite.dev.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig, loadEnv, type ConfigEnv, type UserConfig } from "vite";
-import Inspect from "vite-plugin-inspect";
+// import Inspect from "vite-plugin-inspect";
 
 export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
     const env = loadEnv(mode, process.cwd(), ""); // 暴露所有env文件数据
 
     return {
-        plugins: [Inspect()],
+        // plugins: [Inspect()],
         server: {
             // 是否开启 https
             https: false,
             // 端口号
             port: +env.VITE_PORT,
-            host: "0.0.0.0",
+            host: "0.0.0.0"
             // 本地跨域代理 https://cn.vitejs.dev/config/server-options.html#server-proxy
             // proxy: {
             //     [env.VITE_APP_API_BASE_URL]: {
